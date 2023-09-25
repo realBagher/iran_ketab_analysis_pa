53ba60ef06cc7dcb97bc3877199b58c25db049d0
MadPed <56956134+ped93@users.noreply.github.com>
2023-09-25 15:30:39 +0330

    Update DataBase.py
    Normalize database

--- DataBase.py
+++ DataBase.py
@@ -15,13 +15,13 @@ PASSWORD = '1393ram1393#$'
 SERVER = 'localhost'
 engine = create_engine(f'mysql+pymysql://{USERNAME}:{PASSWORD}@{SERVER}:3306/', echo=True)
 conn = engine.connect()
-database_name = 'IranKetab_scraper'
+database_name = 'IranKetab_scraper1'
 create_database_query = f"CREATE DATABASE {database_name}"
 conn.execute(create_database_query)
 
 
 #%%
-engine = create_engine(f'mysql+pymysql://{USERNAME}:{PASSWORD}@{SERVER}:3306/IranKetab_scraper', echo=True)
+engine = create_engine(f'mysql+pymysql://{USERNAME}:{PASSWORD}@{SERVER}:3306/IranKetab_scraper1', echo=True)
 conn = engine.connect()
 
 
@@ -36,9 +36,22 @@ class Book(Base):
 
     id = Column(Integer, primary_key=True)
     ISBN = Column(String(13), unique=True)
-    persian_name = Column(String(255))
-    english_name = Column(String(255))
+    persian_title = Column(String(255))
+    english_title = Column(String(255))
     rate = Column(Float)
+    price = Column(Float)
+    net_price = Column(Float)
+    discount_percent = Column(Float)
+    current_price = Column(Float)  # for change price in interval time
+    publisher_name = Column(String(32))
+    publisher_id = Column(Integer, unique=True)
+    ISBN13 = Column(String(13))
+    size = Column(String(32))
+    num_page = Column(Integer)
+    cover_type = Column(String(32))
+    print_series = Column(Integer)
+    solarh_py = Column(DateTime)
+    geregorian_py = Column(DateTime)
 
 
 class Person(Base):
@@ -51,42 +64,19 @@ class Person(Base):
     role = Column(String(8))
 
 
-class Price(Base):
-    __tablename__ = 'price'
+class PersonDescription(Base):
+    __tablename__ = 'person_description'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    book_id = Column(Integer, ForeignKey("book.id"))
-    price = Column(Float)
-    net_price = Column(Float)
-    discount_percent = Column(Float)
-    current_price = Column(Float)  # for change price in interval time
+    person_id = Column(Integer, ForeignKey("person.person_id"))
+    description = Column(Text)
 
 
 class Publisher(Base):
     __tablename__ = 'publisher'
-
     id = Column(Integer, primary_key=True, autoincrement=True)
-    book_id = Column(Integer, ForeignKey("book.id"))
+    pub_id = Column(Integer, ForeignKey("book.publisher_id"), unique=True)
     name = Column(String(32))
-
-
-class BookDetails(Base):
-    __tablename__ = 'book_details'
-
-    book_id = Column(Integer, ForeignKey("book.id"), primary_key=True)
-    size = Column(String(32))
-    num_page = Column(Integer)
-    cover_type = Column(String(32))
-
-
-class ReleaseDate(Base):
-    __tablename__ = 'release_date'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    book_id = Column(Integer, ForeignKey("book.id"))
-    shpy = Column(DateTime)  # solar hijri publication year
-    gpy = Column(DateTime)  # gregorian publication year
-    print_series = Column(Integer)  # print series of the book
 
 
 class BookDescription(Base):
@@ -96,19 +86,17 @@ class BookDescription(Base):
     content = Column(Text)
 
 
-class PersonDescription(Base):
-    __tablename__ = 'person_description'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    person_id = Column(Integer, ForeignKey("person.person_id"))
-    description = Column(Text)
-
-
-class BookCategory(Base):
-    __tablename__ = 'book_category'
+class BookTag(Base):
+    __tablename__ = 'book_tag'
 
     book_id = Column(Integer, ForeignKey("book.id"), primary_key=True)
-    categories = Column(String(255))
+    tag_id = Column(Integer,unique=True)
+
+
+class Tag(Base):
+    __tablename__ = 'tag'
+    tag_id = Column(Integer, ForeignKey("book_tag.tag_id"), primary_key=True)
+    tag_title = Column(String(64))
 
 #%%
 
